f3d43c75e40edd59efc30bdac2661af337caefe5
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-09 13:22:06 +0300

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+let currentTime;
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+
+
+player.on('timeupdate', throttle(savedTimeUpdate, 1000));
+
+const getCurrentItem = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(getCurrentItem);
+
+function savedTimeUpdate(data) {
+  currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
