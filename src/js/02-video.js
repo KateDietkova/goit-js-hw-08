2ec2bd48c96657ab1a5fd6763641d87f91768a65
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-09 15:12:50 +0300

    Add task 3 and change variable in task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
 import VimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-let currentTime;
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('#vimeo-player');
 const player = new VimeoPlayer(iframe);
 
 
 player.on('timeupdate', throttle(savedTimeUpdate, 1000));
 
-const getCurrentItem = localStorage.getItem('videoplayer-current-time');
+const getCurrentItem = localStorage.getItem(STORAGE_KEY);
 player.setCurrentTime(getCurrentItem);
 
 function savedTimeUpdate(data) {
-  currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+getFormData();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function getFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const parseSavedData = JSON.parse(savedData);
+    email.value = parseSavedData.email;
+    message.value = parseSavedData.message;
+  }
+}
