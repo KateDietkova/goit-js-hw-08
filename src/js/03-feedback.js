aa3cfb934346850a4dd6de7c07f2d5eafca611ab
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-10 17:24:30 +0300

    fix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,7 +5,7 @@ const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 let savedData;
 
 form.addEventListener('submit', onFormSubmit);
@@ -16,17 +16,27 @@ getFormData();
 function onFormSubmit(event) {
   event.preventDefault();
 
+  if (email.value === '' || message.value === '') {
+    alert('All fields must be filled');
+    return;
+  }
+
   event.currentTarget.reset();
-  console.log(formData);
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+  formData = {};
 }
 
 function onFormInput(event) {
   savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
     const parseSavedData = JSON.parse(savedData);
-    formData.email = parseSavedData.email;
-    formData.message = parseSavedData.message;
+    if (parseSavedData.email !== '') {
+      formData.email = parseSavedData.email;
+    }
+    if (parseSavedData.message !== '') {
+      formData.message = parseSavedData.message;
+    }
 
     formData[event.target.name] = event.target.value;
 
@@ -42,8 +52,13 @@ function getFormData() {
 
   if (savedData) {
     const parseGetData = JSON.parse(savedData);
-    email.value = parseGetData.email;
-    message.value = parseGetData.message;
+
+    if (parseGetData.email) {
+      email.value = parseGetData.email;
+    }
+    if (parseGetData.message) {
+      message.value = parseGetData.message;
+    }
   }
 }
 
